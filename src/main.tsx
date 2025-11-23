b87802832d5d55cab94a06e65d2cd820ead71777
Ahmad Sheikh-Khalil <ahmadsk@Ahmads-MacBook-Air-2.local>
2025-11-23 18:54:33 -0500

    fix favicon and update thumbnail

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,4 @@ import './assets/favicon.ico'
 // Force dark mode
 document.documentElement.classList.add('dark');
 
-// Handle GitHub Pages redirect for client-side routing
-const pathSegmentsToKeep = 0;
-const l = window.location;
-if (l.search) {
-  const search = l.search.slice(1);
-  if (search.startsWith('/')) {
-    const path = search
-      .split('&')[0]
-      .replace(/~and~/g, '&')
-      .split('#')[0];
-    window.history.replaceState(null, '', path + l.hash);
-  }
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
